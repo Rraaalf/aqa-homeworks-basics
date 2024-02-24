f078c1b0137b692f31f698e1871ff8264330d24a
Rraaalf <kymyass@gmail.com>
2024-02-24 14:57:15 +0300

    добавлен тестовый класс CashbackHackServiceTest

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,40 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+    int amount;
+    int actual;
+    int expected;
+
+    @Test
+    public void remainWithAmount1000Test() {
+        amount = 1000;
+        actual = service.remain(amount);
+        expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void remainWithAmount1500Test() {
+        amount = 1500;
+        actual = service.remain(amount);
+        expected = 500;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void remainWithAmount0Test() {
+        amount = 0;
+        actual = service.remain(amount);
+        expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+}
